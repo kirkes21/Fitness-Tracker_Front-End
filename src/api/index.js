64d51e4ea66f4d21f936360b9b93eb869613bdfe
jbiehl88 <jcbiehl8@gmail.com>
2022-04-11 14:14:27 -0500

    fin w/ routines activities

--- src/api/index.js
+++ src/api/index.js
@@ -1,34 +1,47 @@
 // You can choose to import all your functions, and re-export them here
 
-export const baseURL = "https://fitnesstrac-kr.herokuapp.com/api"
+export const baseURL = "https://fitnesstrac-kr.herokuapp.com/api";
 
 export const registerUser = async (username, password) => {
-    const response = await fetch(`${baseURL}/users/register`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            username,
-            password,
-        }),
-    });
-    const data = await response.json();
-    return data;
+  const response = await fetch(`${baseURL}/users/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+  const data = await response.json();
+  return data;
 };
 
 export const loginUser = async (username, password) => {
-    const response = await fetch(`${baseURL}/users/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            username,
-            password,
-        }),
-    });
-    const data = await response.json();
-    return data;
+  const response = await fetch(`${baseURL}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+  const data = await response.json();
+  return data;
 };
 
+export const getActivities = async () => {
+  const response = await fetch(`${baseURL}/activities`);
+  const result = await response.json();
+
+  return result;
+};
+
+export const getRoutines = async () => {
+  const response = await fetch(`${baseURL}/routines`);
+  const result = await response.json();
+
+  return result;
+};
--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getRoutines } from "../api";
 
 const Routines = () => {
-  return <div>Routines</div>;
+  const [routines, setRoutines] = useState([]);
+
+  useEffect(() => {
+    const fetchRoutines = async () => {
+      const routinesFromApi = await getRoutines();
+      setRoutines(routinesFromApi);
+    };
+
+    fetchRoutines();
+  }, []);
+
+  return (
+    <div>
+      {routines.map((routine) => {
+        return (
+          <div key={routine.id}>
+            <h3>Name: {routine.name} </h3>
+            <h5>Goal: {routine.goal}</h5>
+            <h6>Creator: {routine.creatorName}</h6>
+
+            {routine.activities.map((activity) => {
+              return (
+                <div key={activity.id}>
+                  <h3>Activity Name: {activity.name}</h3>
+                  <h6>Description: {activity.description}</h6>
+                  <h6>Duration: {activity.duration}</h6>
+                  <h6>Count: {activity.count}</h6>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Routines;
